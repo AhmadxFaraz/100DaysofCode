dd5b0f26b8149aab36fcd68c09ffcf56c143291a
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-01-14 22:46:19 +0530

    New Project

---
+++ week-4/p.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_left():
+    tim.left(90)
+    tim.forward(10)
+
+def move_right():
+    tim.right(90)
+    tim.forward(10)
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="s", fun=move_right)
+screen.onkey(key="c", fun=clear_screen)
+screen.exitonclick()
