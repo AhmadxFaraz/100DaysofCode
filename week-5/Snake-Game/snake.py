fe042966814a8927bf4ae2d500268b13616aa8c4
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-01-17 16:06:37 +0530

    Complete Snake Game Project

--- week-5/Snake-Game/snake.py
+++ week-5/Snake-Game/snake.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
+
 STARTING_POSITIONS = [(0,0), (-21,0), (-42,0)]
-MOVE_DISTANCE = 20
+
+MOVE_DISTANCE = 21
 
 UP = 90
 DOWN = 270
@@ -16,11 +18,7 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -29,18 +27,29 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.speed("fast")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def up(self):
-        if self.head.heading(UP):
+        if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def down(self):
-        if self.head.heading(DOWN):
+        if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
-        if self.head.setheading(LEFT):
+        if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
-        if self.head.setheading(RIGHT):
+        if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
