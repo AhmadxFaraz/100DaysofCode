1d04d371a417dc501e2d4ae1e27c91e1eb998c06
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-01-22 16:21:26 +0530

    Added High Score Feature

--- week-5/Snake-Game/scoreboard.py
+++ week-5/Snake-Game/scoreboard.py
@@ -13,6 +13,8 @@ class Scoreboard(Turtle):
         self.goto(0,260)
         self.hideturtle()
         self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.write(f"Score: {self.score} High Score: {self.high_score}", align = ALIGNMENT, font = FONT)
 
     def update_scoreboard(self):
@@ -21,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.clear()
         self.update_scoreboard()
