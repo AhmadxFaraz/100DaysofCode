8deaead3eb4b5b519c3f6d02c06156d692d6e556
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-01-30 17:26:59 +0530

    Pomodoro-App Project

---
+++ week-6/Pomodoro-App/main.py
@@ -0,0 +1,105 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+#Colors
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#97be5a"
+BIEGE = "#ffe8c5"
+
+#Font
+FONT_NAME = "Courier"
+
+#Time lengths
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    #timer_text 00:00
+    canvas.itemconfig(timer_text, text="00:00")
+    #timer_label "Timer"
+    timer_label.config(text="Timer")
+    #reset check_marks
+    check_marks.config(text="")
+    #reset reps
+    global reps
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    #If it's the 8th rep:
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    #If it's the 2nd/4th/6th rep:
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    #If it's the 1st/3rd/5th/7th rep:
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count/60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
+# ---------------------------- UI SETUP ------------------------------- #
+#Window Setup
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=40, pady=20, bg=BIEGE)
+
+#Timer Label Setup
+timer_label = Label(text="Timer", bg=BIEGE, fg=GREEN, font=(FONT_NAME, 40, "normal"))
+timer_label.grid(column=1, row=0)
+
+#Canvas & Image Setup
+canvas = Canvas(width=200, height=224, bg=BIEGE, highlightthickness=0)
+tomato_img = PhotoImage(file="/Users/ahmadxfaraz/CodingProjects/100DaysofCode/week-6/Pomodoro-App/tomato.png")
+canvas.create_image(100,112, image=tomato_img)
+timer_text = canvas.create_text(101, 138, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+ 
+#Start button Setup
+start_button = Button(text="Start", highlightthickness=0, bg=BIEGE, command=start_timer)
+start_button.grid(column=0, row=2)
+
+#Reset button Setup
+reset_button = Button(text="Reset", highlightthickness=0, bg=BIEGE, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+#Check-Marks setup
+check_marks = Label(bg=BIEGE, highlightthickness=0)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
