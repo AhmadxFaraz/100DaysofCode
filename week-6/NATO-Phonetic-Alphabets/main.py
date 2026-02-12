2d1179b35ae93d3b082e066812c5f544aacca8f1
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-02-12 16:46:34 +0530

    added error & exception handling

---
+++ week-6/NATO-Phonetic-Alphabets/main.py
@@ -0,0 +1,17 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+
+invalid = True
+
+while invalid:
+    word = input("Enter a word: ").upper()
+
+    try:
+        new = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        invalid = True
+    else:
+        print(new)
+        invalid = False
