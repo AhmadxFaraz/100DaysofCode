dc4202887f62a62cbffa497902a2c6acf54a9783
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-03-12 13:47:41 +0530

    Time correction

--- week-11/Quiz-App-API/ui.py
+++ week-11/Quiz-App-API/ui.py
@@ -1,2 +1,19 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(width=300, height=250,bg="#FFFFFF", highlightthickness=0)
+        self.canvas.grid(row=1, column=0, columnspan=2)
+
+        self.true_img = PhotoImage(file="/images/true.png")
+        self.false_img = PhotoImage(file="/images/false.png")
+
+
+        self.window.mainloop()
